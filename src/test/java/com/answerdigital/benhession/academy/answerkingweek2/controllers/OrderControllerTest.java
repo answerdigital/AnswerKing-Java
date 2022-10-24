16a1d69da89d5a987d0c722a1f88042cb9ac9e26
Martin Taylor <martintaylor1635@gmail.com>
2022-10-24 10:10:59 +0100

    BENCH-57 Added boilerplate tests for the order controller.

---
+++ src/test/java/com/answerdigital/benhession/academy/answerkingweek2/controllers/OrderControllerTest.java
@@ -0,0 +1,109 @@
+package com.answerdigital.benhession.academy.answerkingweek2.controllers;
+
+import com.answerdigital.benhession.academy.answerkingweek2.request.AddOrderRequest;
+import com.answerdigital.benhession.academy.answerkingweek2.services.OrderService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+
+@ContextConfiguration(classes = {OrderController.class})
+@ExtendWith(SpringExtension.class)
+public class OrderControllerTest {
+
+    @Autowired
+    private OrderController orderController;
+
+    @MockBean
+    private OrderService orderService;
+
+    /**
+     * Method under test: {@link OrderController#addItemToBasket(Long, Long, Integer)}
+     */
+    @Test
+    void testAddItemToBasket() throws Exception {
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/{orderId}/item/{itemId}/quantity/{quantity}", 123L, 123L, 1);
+        ResultActions actualPerformResult = MockMvcBuilders.standaloneSetup(orderController)
+                .build()
+                .perform(requestBuilder);
+        actualPerformResult.andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    /**
+     * Method under test: {@link OrderController#addOrder(AddOrderRequest)}
+     */
+    @Test
+    void testAddOrder() throws Exception {
+        MockHttpServletRequestBuilder contentTypeResult = MockMvcRequestBuilders.get("/")
+                .contentType(MediaType.APPLICATION_JSON);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        MockHttpServletRequestBuilder requestBuilder = contentTypeResult
+                .content(objectMapper.writeValueAsString(new AddOrderRequest("42 Main St")));
+        ResultActions actualPerformResult = MockMvcBuilders.standaloneSetup(orderController)
+                .build()
+                .perform(requestBuilder);
+        actualPerformResult.andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    /**
+     * Method under test: {@link OrderController#deleteItemInBasket(Long, Long)}
+     */
+    @Test
+    void testDeleteItemInBasket() throws Exception {
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.delete("/{orderId}/item/{itemId}", 123L,
+                123L);
+        ResultActions actualPerformResult = MockMvcBuilders.standaloneSetup(orderController)
+                .build()
+                .perform(requestBuilder);
+        actualPerformResult.andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    /**
+     * Method under test: {@link OrderController#getAllOrders()}
+     */
+    @Test
+    void testGetAllOrders() throws Exception {
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/");
+        ResultActions actualPerformResult = MockMvcBuilders.standaloneSetup(orderController)
+                .build()
+                .perform(requestBuilder);
+        actualPerformResult.andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    /**
+     * Method under test: {@link OrderController#getOrder(Long)}
+     */
+    @Test
+    void testGetOrder() throws Exception {
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/{orderId}", 123L);
+        ResultActions actualPerformResult = MockMvcBuilders.standaloneSetup(orderController)
+                .build()
+                .perform(requestBuilder);
+        actualPerformResult.andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    /**
+     * Method under test: {@link OrderController#updateItemQuantity(Long, Long, Integer)}
+     */
+    @Test
+    void testUpdateItemQuantity() throws Exception {
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
+                .put("/{orderId}/item/{itemId}/quantity/{quantity}", 123L, 123L, 1);
+        ResultActions actualPerformResult = MockMvcBuilders.standaloneSetup(orderController)
+                .build()
+                .perform(requestBuilder);
+        actualPerformResult.andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+}
