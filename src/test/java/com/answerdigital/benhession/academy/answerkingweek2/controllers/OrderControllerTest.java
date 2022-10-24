64bffc75d955e0cfacdba36bcf0463da3bb48258
Martin Taylor <martintaylor1635@gmail.com>
2022-10-24 10:10:59 +0100

    BENCH-57 Added boilerplate tests for the order controller.

--- src/test/java/com/answerdigital/benhession/academy/answerkingweek2/controllers/OrderControllerTest.java
+++ src/test/java/com/answerdigital/benhession/academy/answerkingweek2/controllers/OrderControllerTest.java
@@ -1,294 +1,109 @@
 package com.answerdigital.benhession.academy.answerkingweek2.controllers;
 
+import com.answerdigital.benhession.academy.answerkingweek2.request.AddOrderRequest;
+import com.answerdigital.benhession.academy.answerkingweek2.services.OrderService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 
-@ExtendWith(MockitoExtension.class)
+@ContextConfiguration(classes = {OrderController.class})
+@ExtendWith(SpringExtension.class)
 public class OrderControllerTest {
 
-//    @Mock
-//    OrderService orderService;
-//    @Mock
-//    ItemService itemService;
-//    @Mock
-//    BindingResult bindingResult;
-//
-//    @Test
-//    void addOrder_verificationHasErrors_throwsInvalidValues() {
-//        OrderController orderController = new OrderController(orderService, itemService);
-//        AddOrderDTO addOrderDTO = new AddOrderDTO("");
-//
-//        when(bindingResult.hasErrors()).thenThrow(new InvalidValuesException());
-//
-//        Assertions.assertThrows(InvalidValuesException.class,
-//                () -> orderController.addOrder(addOrderDTO, bindingResult));
-//    }
-//
-//    @Test
-//    void addOrder_validDTO_httpStatusOK(@Mock Order order) {
-//        OrderController orderController = new OrderController(orderService, itemService);
-//        AddOrderDTO addOrderDTO = new AddOrderDTO("9 Dewsbury Rd, Leeds LS11 5DD");
-//
-//        when(bindingResult.hasErrors()).thenReturn(false);
-//        when(orderService.addOrder(notNull())).thenReturn(Optional.of(order));
-//
-//        Assertions.assertEquals(HttpStatus.CREATED,
-//                orderController.addOrder(addOrderDTO, bindingResult).getStatusCode());
-//    }
-//
-//    @Test
-//    void addOrder_saveFailed_throwsUnableToSave() {
-//        OrderController orderController = new OrderController(orderService, itemService);
-//        AddOrderDTO addOrderDTO = new AddOrderDTO("9 Dewsbury Rd, Leeds LS11 5DD");
-//
-//        when(bindingResult.hasErrors()).thenReturn(false);
-//
-//        Assertions.assertThrows(UnableToSaveEntityException.class,
-//                () -> orderController.addOrder(addOrderDTO, bindingResult));
-//    }
-//
-//    @Test
-//    void getOrder_orderNotFound_throwsNotFound() {
-//        OrderController orderController = new OrderController(orderService, itemService);
-//
-//        when(orderService.findById(anyInt())).thenReturn(Optional.empty());
-//
-//        Assertions.assertThrows(NotFoundException.class,
-//                () -> orderController.getOrder(1));
-//    }
-//
-//    @Test
-//    void getOrder_orderFound_httpStatusOK(@Mock Order order) {
-//        OrderController orderController = new OrderController(orderService, itemService);
-//
-//        when(orderService.findById(anyInt())).thenReturn(Optional.of(order));
-//
-//        Assertions.assertEquals(HttpStatus.OK,
-//                orderController.getOrder(1).getStatusCode());
-//
-//    }
-//
-//    @Test
-//    void addItemToBasket_verificationHasErrors_throwsInvalidValues(@Mock ItemToBasketDTO itemToBasketDTO) {
-//        OrderController orderController = new OrderController(orderService, itemService);
-//
-//        when(bindingResult.hasErrors()).thenThrow(new InvalidValuesException());
-//
-//        Assertions.assertThrows(InvalidValuesException.class,
-//                () -> orderController.addItemToBasket(1, itemToBasketDTO, bindingResult));
-//    }
-//
-//    @Test
-//    void addItemToBasket_orderNotFound_throwsNotFound(@Mock ItemToBasketDTO itemToBasketDTO,
-//                                                      @Mock Item item) {
-//        OrderController orderController = new OrderController(orderService, itemService);
-//
-//        when(bindingResult.hasErrors()).thenReturn(false);
-//        when(orderService.findById(anyInt())).thenReturn(Optional.empty());
-//        when(itemService.findById(anyInt())).thenReturn(Optional.of(item));
-//        when(itemToBasketDTO.getQuantity()).thenReturn(1);
-//
-//        Assertions.assertThrows(NotFoundException.class,
-//                () -> orderController.addItemToBasket(1, itemToBasketDTO, bindingResult));
-//    }
-//
-//    @Test
-//    void addItemToBasket_itemNotFound_throwsNotFound(@Mock ItemToBasketDTO itemToBasketDTO,
-//                                                     @Mock Order order) {
-//        OrderController orderController = new OrderController(orderService, itemService);
-//
-//        when(bindingResult.hasErrors()).thenReturn(false);
-//        when(orderService.findById(anyInt())).thenReturn(Optional.of(order));
-//        when(itemService.findById(anyInt())).thenReturn(Optional.empty());
-//        when(itemToBasketDTO.getQuantity()).thenReturn(1);
-//
-//        Assertions.assertThrows(NotFoundException.class,
-//                () -> orderController.addItemToBasket(1, itemToBasketDTO, bindingResult));
-//    }
-//
-//    @Test
-//    void addItemToBasket_itemUnavailable_throwsItemUnavailable(@Mock ItemToBasketDTO itemToBasketDTO,
-//                                                               @Mock Order order,
-//                                                               @Mock Item item) {
-//        OrderController orderController = new OrderController(orderService, itemService);
-//
-//        when(bindingResult.hasErrors()).thenReturn(false);
-//        when(orderService.findById(anyInt())).thenReturn(Optional.of(order));
-//        when(itemService.findById(anyInt())).thenReturn(Optional.of(item));
-//        when(itemToBasketDTO.getQuantity()).thenReturn(1);
-//        when(item.isAvailable()).thenReturn(false);
-//
-//        Assertions.assertThrows(ItemUnavailableException.class,
-//                () -> orderController.addItemToBasket(1, itemToBasketDTO, bindingResult));
-//    }
-//
-//    @Test
-//    void addItemToBasket_itemAlreadyInOrder_throwsConflict(@Mock ItemToBasketDTO itemToBasketDTO,
-//                                                           @Mock Order order,
-//                                                           @Mock Item item) {
-//        OrderController orderController = new OrderController(orderService, itemService);
-//
-//        when(bindingResult.hasErrors()).thenReturn(false);
-//        when(orderService.findById(anyInt())).thenReturn(Optional.of(order));
-//        when(itemService.findById(anyInt())).thenReturn(Optional.of(item));
-//        when(itemToBasketDTO.getQuantity()).thenReturn(1);
-//        when(item.isAvailable()).thenReturn(true);
-//        when(order.addItemToBasket(item, itemToBasketDTO.getQuantity())).thenReturn(false);
-//
-//        Assertions.assertThrows(ConflictException.class,
-//                () -> orderController.addItemToBasket(1, itemToBasketDTO, bindingResult));
-//    }
-//
-//    @Test
-//    void addItemToBasket_newItem_httpStatusOK(@Mock ItemToBasketDTO itemToBasketDTO,
-//                                              @Mock Order order,
-//                                              @Mock Item item) {
-//        OrderController orderController = new OrderController(orderService, itemService);
-//
-//        when(bindingResult.hasErrors()).thenReturn(false);
-//        when(orderService.findById(anyInt())).thenReturn(Optional.of(order));
-//        when(itemService.findById(anyInt())).thenReturn(Optional.of(item));
-//        when(orderService.update(order)).thenReturn(order);
-//        when(itemToBasketDTO.getQuantity()).thenReturn(1);
-//        when(item.isAvailable()).thenReturn(true);
-//        when(order.addItemToBasket(item, itemToBasketDTO.getQuantity())).thenReturn(true);
-//        when(order.getId()).thenReturn(1);
-//
-//        Assertions.assertEquals(HttpStatus.CREATED, orderController.addItemToBasket(1, itemToBasketDTO,
-//                bindingResult).getStatusCode());
-//    }
-//
-//    @Test
-//    void deleteItemInBasket_itemNotFound_throwsNotFound(@Mock Order order) {
-//        OrderController orderController = new OrderController(orderService, itemService);
-//
-//        when(orderService.findById(anyInt())).thenReturn(Optional.of(order));
-//        when(itemService.findById(anyInt())).thenReturn(Optional.empty());
-//
-//        Assertions.assertThrows(NotFoundException.class,
-//                () -> orderController.deleteItemInBasket(1, 1).getStatusCode());
-//
-//    }
-//
-//    @Test
-//    void deleteItemInBasket_orderNotFound_throwsNotFound(@Mock Item item) {
-//        OrderController orderController = new OrderController(orderService, itemService);
-//
-//        when(orderService.findById(anyInt())).thenReturn(Optional.empty());
-//        when(itemService.findById(anyInt())).thenReturn(Optional.of(item));
-//
-//        Assertions.assertThrows(NotFoundException.class,
-//                () -> orderController.deleteItemInBasket(1, 1).getStatusCode());
-//
-//    }
-//
-//    @Test
-//    void deleteItemInBasket_itemNotInBasket_throwsNotFound(@Mock Item item, @Mock Order order) {
-//        OrderController orderController = new OrderController(orderService, itemService);
-//
-//        when(orderService.findById(anyInt())).thenReturn(Optional.of(order));
-//        when(itemService.findById(anyInt())).thenReturn(Optional.of(item));
-//        when(order.basketHasItem(item)).thenReturn(false);
-//
-//        Assertions.assertThrows(NotFoundException.class,
-//                () -> orderController.deleteItemInBasket(1, 1).getStatusCode());
-//
-//    }
-//
-//    @Test
-//    void deleteItemInBasket_validItem_httpStatusOK(@Mock Item item, @Mock Order order) {
-//        OrderController orderController = new OrderController(orderService, itemService);
-//
-//        when(orderService.findById(anyInt())).thenReturn(Optional.of(order));
-//        when(itemService.findById(anyInt())).thenReturn(Optional.of(item));
-//        when(order.basketHasItem(item)).thenReturn(true);
-//        when(orderService.update(order)).thenReturn(order);
-//
-//        Assertions.assertEquals(HttpStatus.OK,
-//                orderController.deleteItemInBasket(1, 1).getStatusCode());
-//
-//    }
-//
-//    @Test
-//    void updateItemQuantity_validationErrors_throwsInvalidValues(@Mock ItemQuantityDTO itemQuantityDTO) {
-//        OrderController orderController = new OrderController(orderService, itemService);
-//
-//        when(bindingResult.hasErrors()).thenThrow(new InvalidValuesException());
-//
-//        Assertions.assertThrows(InvalidValuesException.class,
-//                () -> orderController.updateItemQuantity(1, 2, itemQuantityDTO, bindingResult));
-//    }
-//
-//    @Test
-//    void updateItemQuantity_orderNotFound_throwsNotFound(@Mock Item item, @Mock ItemQuantityDTO itemQuantityDTO) {
-//        OrderController orderController = new OrderController(orderService, itemService);
-//
-//        when(orderService.findById(anyInt())).thenReturn(Optional.empty());
-//        when(itemService.findById(anyInt())).thenReturn(Optional.of(item));
-//
-//        Assertions.assertThrows(NotFoundException.class,
-//                () -> orderController.updateItemQuantity(1, 2, itemQuantityDTO, bindingResult));
-//
-//
-//    }
-//
-//    @Test
-//    void updateItemQuantity_itemNotFound_throwsNotFound(@Mock Order order, @Mock ItemQuantityDTO itemQuantityDTO) {
-//        OrderController orderController = new OrderController(orderService, itemService);
-//
-//        when(orderService.findById(anyInt())).thenReturn(Optional.of(order));
-//        when(itemService.findById(anyInt())).thenReturn(Optional.empty());
-//
-//        Assertions.assertThrows(NotFoundException.class,
-//                () -> orderController.updateItemQuantity(1, 2, itemQuantityDTO, bindingResult));
-//
-//    }
-//
-//    @Test
-//    void updateItemQuantity_itemNotInBasket_throwsNotFound(@Mock Order order,
-//                                                           @Mock Item item,
-//                                                           @Mock ItemQuantityDTO itemQuantityDTO) {
-//        OrderController orderController = new OrderController(orderService, itemService);
-//
-//        when(orderService.findById(anyInt())).thenReturn(Optional.of(order));
-//        when(itemService.findById(anyInt())).thenReturn(Optional.of(item));
-//        when(order.basketHasItem(item)).thenReturn(false);
-//
-//        Assertions.assertThrows(NotFoundException.class,
-//                () -> orderController.updateItemQuantity(1, 2, itemQuantityDTO, bindingResult));
-//    }
-//
-//    @Test
-//    void updateItemQuantity_valid_HttpStatusOK(@Mock Order order,
-//                                                           @Mock Item item,
-//                                                           @Mock ItemQuantityDTO itemQuantityDTO) {
-//        OrderController orderController = new OrderController(orderService, itemService);
-//
-//        when(orderService.findById(anyInt())).thenReturn(Optional.of(order));
-//        when(itemService.findById(anyInt())).thenReturn(Optional.of(item));
-//        when(order.basketHasItem(item)).thenReturn(true);
-//        when(orderService.update(order)).thenReturn(order);
-//
-//        Assertions.assertEquals(HttpStatus.OK,
-//                orderController.updateItemQuantity(1, 2, itemQuantityDTO, bindingResult).getStatusCode());
-//    }
-//
-//    @Test
-//    void getAllOrders_ordersPresent_httpStatusOK(@Mock List<Order> orders) {
-//        OrderController orderController = new OrderController(orderService, itemService);
-//
-//        when(orderService.getAll()).thenReturn(Optional.of(orders));
-//
-//        Assertions.assertEquals(HttpStatus.OK, orderController.getAllOrders().getStatusCode());
-//    }
-//
-//    @Test
-//    void getAllOrders_noOrders_httpStatusNoContent() {
-//        OrderController orderController = new OrderController(orderService, itemService);
-//
-//        when(orderService.getAll()).thenReturn(Optional.empty());
-//
-//        Assertions.assertEquals(HttpStatus.NO_CONTENT, orderController.getAllOrders().getStatusCode());
-//    }
+    @Autowired
+    private OrderController orderController;
+
+    @MockBean
+    private OrderService orderService;
+
+    /**
+     * Method under test: {@link OrderController#addItemToBasket(Long, Long, Integer)}
+     */
+    @Test
+    void testAddItemToBasket() throws Exception {
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/{orderId}/item/{itemId}/quantity/{quantity}", 123L, 123L, 1);
+        ResultActions actualPerformResult = MockMvcBuilders.standaloneSetup(orderController)
+                .build()
+                .perform(requestBuilder);
+        actualPerformResult.andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    /**
+     * Method under test: {@link OrderController#addOrder(AddOrderRequest)}
+     */
+    @Test
+    void testAddOrder() throws Exception {
+        MockHttpServletRequestBuilder contentTypeResult = MockMvcRequestBuilders.get("/")
+                .contentType(MediaType.APPLICATION_JSON);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        MockHttpServletRequestBuilder requestBuilder = contentTypeResult
+                .content(objectMapper.writeValueAsString(new AddOrderRequest("42 Main St")));
+        ResultActions actualPerformResult = MockMvcBuilders.standaloneSetup(orderController)
+                .build()
+                .perform(requestBuilder);
+        actualPerformResult.andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    /**
+     * Method under test: {@link OrderController#deleteItemInBasket(Long, Long)}
+     */
+    @Test
+    void testDeleteItemInBasket() throws Exception {
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.delete("/{orderId}/item/{itemId}", 123L,
+                123L);
+        ResultActions actualPerformResult = MockMvcBuilders.standaloneSetup(orderController)
+                .build()
+                .perform(requestBuilder);
+        actualPerformResult.andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    /**
+     * Method under test: {@link OrderController#getAllOrders()}
+     */
+    @Test
+    void testGetAllOrders() throws Exception {
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/");
+        ResultActions actualPerformResult = MockMvcBuilders.standaloneSetup(orderController)
+                .build()
+                .perform(requestBuilder);
+        actualPerformResult.andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    /**
+     * Method under test: {@link OrderController#getOrder(Long)}
+     */
+    @Test
+    void testGetOrder() throws Exception {
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/{orderId}", 123L);
+        ResultActions actualPerformResult = MockMvcBuilders.standaloneSetup(orderController)
+                .build()
+                .perform(requestBuilder);
+        actualPerformResult.andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    /**
+     * Method under test: {@link OrderController#updateItemQuantity(Long, Long, Integer)}
+     */
+    @Test
+    void testUpdateItemQuantity() throws Exception {
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
+                .put("/{orderId}/item/{itemId}/quantity/{quantity}", 123L, 123L, 1);
+        ResultActions actualPerformResult = MockMvcBuilders.standaloneSetup(orderController)
+                .build()
+                .perform(requestBuilder);
+        actualPerformResult.andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
 }
