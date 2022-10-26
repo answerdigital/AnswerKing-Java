3a697b66ffee74d1db63dc32dd8105dc54ef603a
Oleksii <oleksi.rybak@gmail.com>
2022-10-26 14:29:38 +0100

    reverting the deleted changes

--- src/main/java/com/answerdigital/benhession/academy/answerkingweek2/config/SecurityConfig.java
+++ src/main/java/com/answerdigital/benhession/academy/answerkingweek2/config/SecurityConfig.java
@@ -1,36 +1,53 @@
 package com.answerdigital.benhession.academy.answerkingweek2.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+public class SecurityConfig {
 
-    @Override
-    protected void configure(final HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .authorizeRequests().anyRequest().authenticated()
-                .and()
-                .httpBasic()
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    @Bean
+    public SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
+            http
+            .csrf().disable()
+            .authorizeRequests().anyRequest().authenticated()
+            .and()
+            .httpBasic()
+            .and()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+            return http.build();
     }
 
-    @Override
-    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .withUser("paul").password("{noop}secret").authorities("ROLE_USER")
-                .and()
-                .withUser("john").password("{noop}secret").authorities("ROLE_USER")
-                .and()
-                .withUser("ringo").password("{noop}secret").authorities("ROLE_USER")
-                .and()
-                .withUser("george").password("{noop}secret").authorities("ROLE_USER");
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        final UserDetails paul = User.withUsername("paul")
+                .password("{noop}secret")
+                .authorities("ROLE_USER")
+                .build();
+
+        final UserDetails john = User.withUsername("john")
+                .password("{noop}secret")
+                .authorities("ROLE_USER")
+                .build();
+
+        final UserDetails ringo = User.withUsername("ringo")
+                .password("{noop}secret")
+                .authorities("ROLE_USER")
+                .build();
+
+        final UserDetails george = User.withUsername("george")
+                .password("{noop}secret")
+                .authorities("ROLE_USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(paul, john, ringo, george);
     }
 }
