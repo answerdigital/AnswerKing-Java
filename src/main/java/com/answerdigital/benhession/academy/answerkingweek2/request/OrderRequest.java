5022e25ea20985a62965e8d3a6d3920759335029
Martin Taylor <martintaylor1635@gmail.com>
2022-10-27 17:12:17 +0100

    BENCH-63 Renamed AddOrderRequest.java to OrderRequest.java

--- src/main/java/com/answerdigital/benhession/academy/answerkingweek2/request/OrderRequest.java
+++ src/main/java/com/answerdigital/benhession/academy/answerkingweek2/request/OrderRequest.java
@@ -2,5 +2,5 @@ package com.answerdigital.benhession.academy.answerkingweek2.request;
 
 import javax.validation.constraints.NotBlank;
 
-public record AddOrderRequest(@NotBlank String address) {
+public record OrderRequest(@NotBlank String address) {
 }
