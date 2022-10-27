3392e54757a28602a89b02f043efe137692f83ff
AudriusPutys <audrius.putys@answerdigital.com>
2022-10-27 14:20:40 +0100

    BENCH-46 Lombok Implemented and controller test updated to bypass security

--- src/main/java/com/answerdigital/benhession/academy/answerkingweek2/model/Order.java
+++ src/main/java/com/answerdigital/benhession/academy/answerkingweek2/model/Order.java
@@ -1,25 +1,27 @@
 package com.answerdigital.benhession.academy.answerkingweek2.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import javax.persistence.Table;
 import javax.persistence.Id;
-import javax.persistence.Enumerated;
-import javax.persistence.EnumType;
 import javax.persistence.OneToMany;
-import javax.persistence.CascadeType;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
-import java.util.Comparator;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-import java.util.Objects;
 
 @Entity
 @Table(name = "order")
+@NoArgsConstructor
+@Getter
+@Setter
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -34,65 +36,8 @@ public class Order {
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<OrderItem> orderItems = new HashSet<>();
 
-    public Order() {
-    }
-
     public Order(final String address) {
         this.address = address;
         this.orderStatus = OrderStatus.IN_PROGRESS;
     }
-
-    public void setOrderItems(final Set<OrderItem> orderItems) {
-        this.orderItems = orderItems;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public OrderStatus getOrderStatus() {
-        return orderStatus;
-    }
-
-    public void setAddress(final String address) {
-        this.address = address;
-    }
-
-    public void setOrderStatus(final OrderStatus orderStatus) {
-        this.orderStatus = orderStatus;
-    }
-
-    public List<OrderItem> getOrderItems() {
-        return orderItems
-                .stream()
-                .sorted(Comparator.comparing(OrderItem::getId))
-                .toList();
-    }
-
-    @JsonIgnore
-    public Set<OrderItem> getOrderItemsSet() {
-        return orderItems;
-    }
-
-    public void setId(final Long id) {
-        this.id = id;
-    }
-
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        final Order order = (Order) o;
-        return id.equals(order.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
 }
