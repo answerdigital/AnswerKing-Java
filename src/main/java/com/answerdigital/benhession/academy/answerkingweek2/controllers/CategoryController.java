8a06fd39a863ed937746742c5b550b7bd00948d9
Oleksii <oleksi.rybak@gmail.com>
2022-10-27 19:14:39 +0100

    putting a proper annotation for class level

--- src/main/java/com/answerdigital/benhession/academy/answerkingweek2/controllers/CategoryController.java
+++ src/main/java/com/answerdigital/benhession/academy/answerkingweek2/controllers/CategoryController.java
@@ -7,6 +7,7 @@ import com.answerdigital.benhession.academy.answerkingweek2.services.CategorySer
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,12 +17,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 
-import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import java.util.Collection;
 import java.util.Set;
 
-@Valid
+@Validated
 @RestController
 @RequestMapping(path = "/category")
 public class CategoryController {
--- src/main/java/com/answerdigital/benhession/academy/answerkingweek2/controllers/ItemController.java
+++ src/main/java/com/answerdigital/benhession/academy/answerkingweek2/controllers/ItemController.java
@@ -6,6 +6,7 @@ import com.answerdigital.benhession.academy.answerkingweek2.services.ItemService
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -18,7 +19,7 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
-@Valid
+@Validated
 @RestController
 @RequestMapping(path = "/item")
 public class ItemController {
