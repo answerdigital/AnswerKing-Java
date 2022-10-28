fad4fd1e991365f718f1efdeda468e4e4fb92546
Oleksii <oleksi.rybak@gmail.com>
2022-10-28 11:48:19 +0100

    enabling lombok with mapstruct

--- src/main/java/com/answerdigital/benhession/academy/answerkingweek2/services/ItemService.java
+++ src/main/java/com/answerdigital/benhession/academy/answerkingweek2/services/ItemService.java
@@ -10,7 +10,6 @@ import org.mapstruct.factory.Mappers;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
