f6fd8e96a7a921c5249951ba6a6450690de00a95
Oleksii <oleksi.rybak@gmail.com>
2022-10-28 01:01:28 +0100

    standardizing API code

--- src/main/java/com/answerdigital/benhession/academy/answerkingweek2/controllers/CategoryController.java
+++ src/main/java/com/answerdigital/benhession/academy/answerkingweek2/controllers/CategoryController.java
@@ -7,6 +7,7 @@ import com.answerdigital.benhession.academy.answerkingweek2.services.CategorySer
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import java.util.Collection;
 import java.util.Set;
@@ -33,8 +35,10 @@ public class CategoryController {
     }
 
     @PostMapping
-    public ResponseEntity<Category> addCategory(@RequestBody final AddCategoryRequest categoryRequest) {
-        return ResponseEntity.ok(categoryService.addCategory(categoryRequest));
+    public ResponseEntity<Category> addCategory(@Valid @RequestBody final AddCategoryRequest categoryRequest,
+                                                final Errors errors) {
+        return new ResponseEntity<>(categoryService.addCategory(categoryRequest),
+                                                        errors.hasErrors() ? HttpStatus.BAD_REQUEST : HttpStatus.OK);
     }
 
     @GetMapping
@@ -46,19 +50,21 @@ public class CategoryController {
     @PutMapping("/{categoryId}/additem/{itemId}")
     public ResponseEntity<Category> addItemToCategory(@PathVariable @NotNull final Long categoryId,
                                                       @PathVariable @NotNull final Long itemId) {
-        return ResponseEntity.ok(categoryService.addItemToCategory(categoryId, itemId));
+        return new ResponseEntity<>(categoryService.addItemToCategory(categoryId, itemId), HttpStatus.OK);
     }
 
     @PutMapping("/{categoryId}/removeitem/{itemId}")
     public ResponseEntity<Category> removeItemFromCategory(@PathVariable @NotNull final Long categoryId,
                                                            @PathVariable @NotNull final Long itemId) {
-        return ResponseEntity.ok(categoryService.removeItemFromCategory(categoryId, itemId));
+        return new ResponseEntity<>(categoryService.removeItemFromCategory(categoryId, itemId), HttpStatus.OK);
     }
 
     @PutMapping("/{categoryId}")
-    public ResponseEntity<Category> updateCategory(@RequestBody final UpdateCategoryRequest updateCategoryRequest,
-                                                   @PathVariable @NotNull final Long categoryId) {
-        return ResponseEntity.ok(categoryService.updateCategory(updateCategoryRequest, categoryId));
+    public ResponseEntity<Category> updateCategory(@Valid @RequestBody final UpdateCategoryRequest updateCategoryRequest,
+                                                   @PathVariable @NotNull final Long categoryId,
+                                                   final Errors errors) {
+        return new ResponseEntity<>(categoryService.updateCategory(updateCategoryRequest, categoryId),
+                                                    errors.hasErrors() ? HttpStatus.BAD_REQUEST : HttpStatus.OK);
     }
 
     @DeleteMapping("/{categoryId}")
--- src/main/java/com/answerdigital/benhession/academy/answerkingweek2/controllers/OrderController.java
+++ src/main/java/com/answerdigital/benhession/academy/answerkingweek2/controllers/OrderController.java
@@ -6,6 +6,7 @@ import com.answerdigital.benhession.academy.answerkingweek2.services.OrderServic
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,13 +34,14 @@ public class OrderController {
     }
 
     @PostMapping
-    public ResponseEntity<Order> addOrder(@Valid @RequestBody final AddOrderRequest addOrderRequest) {
-        return new ResponseEntity<>(orderService.addOrder(addOrderRequest.address()), HttpStatus.CREATED);
+    public ResponseEntity<Order> addOrder(@Valid @RequestBody final AddOrderRequest addOrderRequest, final Errors errors) {
+        return new ResponseEntity<>(orderService.addOrder(addOrderRequest.address()),
+                                            errors.hasErrors() ? HttpStatus.BAD_REQUEST : HttpStatus.CREATED);
     }
 
     @GetMapping(path = "/{orderId}")
     public ResponseEntity<Order> getOrder(@PathVariable @NotNull final Long orderId) {
-        return ResponseEntity.ok(orderService.findById(orderId));
+        return new ResponseEntity<>(orderService.findById(orderId), HttpStatus.CREATED);
     }
 
     @GetMapping
@@ -52,19 +54,19 @@ public class OrderController {
     public ResponseEntity<Order> addItemToBasket(@PathVariable @NotNull final Long orderId,
                                                  @PathVariable @NotNull final Long itemId,
                                                  @PathVariable @NotNull final Integer quantity) {
-        return ResponseEntity.ok(orderService.addItemToBasket(orderId, itemId, quantity));
+        return new ResponseEntity<>(orderService.addItemToBasket(orderId, itemId, quantity), HttpStatus.OK);
     }
 
     @DeleteMapping(path = "/{orderId}/item/{itemId}")
     public ResponseEntity<Order> deleteItemInBasket(@PathVariable @NotNull final Long orderId,
                                                     @PathVariable @NotNull final Long itemId) {
-        return ResponseEntity.ok(orderService.deleteItemInBasket(orderId, itemId));
+        return new ResponseEntity<>(orderService.deleteItemInBasket(orderId, itemId), HttpStatus.OK);
     }
 
     @PutMapping(path = "/{orderId}/item/{itemId}/quantity/{quantity}")
     public ResponseEntity<Order> updateItemQuantity(@PathVariable @NotNull final Long orderId,
                                                     @PathVariable @NotNull final Long itemId,
                                                     @PathVariable @NotNull final Integer quantity) {
-        return ResponseEntity.ok(orderService.updateItemQuantity(orderId, itemId, quantity));
+        return new ResponseEntity<>(orderService.updateItemQuantity(orderId, itemId, quantity), HttpStatus.OK);
     }
 }
