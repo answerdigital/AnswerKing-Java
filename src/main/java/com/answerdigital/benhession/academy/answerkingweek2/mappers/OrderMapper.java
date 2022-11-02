02e9e442b634293db9cb31dc25fe11342353045a
hospel <oleksi.rybak@gmail.com>
2022-11-02 12:40:20 +0000

    fixing order adding issue (#24)

--- src/main/java/com/answerdigital/benhession/academy/answerkingweek2/mappers/OrderMapper.java
+++ src/main/java/com/answerdigital/benhession/academy/answerkingweek2/mappers/OrderMapper.java
@@ -3,10 +3,13 @@ package com.answerdigital.benhession.academy.answerkingweek2.mappers;
 import com.answerdigital.benhession.academy.answerkingweek2.model.Order;
 import com.answerdigital.benhession.academy.answerkingweek2.request.OrderRequest;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface OrderMapper {
+
+    @Mapping(target = "orderStatus", constant = "IN_PROGRESS")
     Order addRequestToOrder(OrderRequest orderRequest);
 
     Order updateOrderRequest(@MappingTarget Order order, OrderRequest orderRequest);
