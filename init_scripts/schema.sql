f19b1ae7191ff6a75f6f364b774cd3b6d489c9f1
martintaylor1635 <martintaylor1635@gmail.com>
2022-11-11 12:05:06 +0000

    BENCH-153 Updated SQL files to reflect new retired fields added in models.

--- init_scripts/schema.sql
+++ init_scripts/schema.sql
@@ -3,15 +3,17 @@ CREATE TABLE IF NOT EXISTS product (
     name VARCHAR(255) NOT NULL,
     description VARCHAR(255) NOT NULL,
     available BIT(1) NOT NULL,
+    retired BIT(1) NOT NULL,
     price DECIMAL(12,2) NOT NULL,
 
     PRIMARY KEY (id)
-    );
+);
 
 CREATE TABLE IF NOT EXISTS category (
     id BIGINT NOT NULL AUTO_INCREMENT,
     name VARCHAR(255),
     description VARCHAR(255) NOT NULL,
+    retired BIT(1) NOT NULL,
 
     PRIMARY KEY (id)
 );
