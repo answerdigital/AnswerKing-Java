e24e499673b0f81e1f7c31196e649cc4bf4f17ff
martintaylor1635 <martintaylor1635@gmail.com>
2022-11-14 14:39:33 +0000

    BENCH-153 Explicitly configured the ProductMapper to default the retired field to false when mapping a ProductRequest to a Product

--- src/main/java/com/answerdigital/academy/answerking/mapper/ProductMapper.java
+++ src/main/java/com/answerdigital/academy/answerking/mapper/ProductMapper.java
@@ -3,10 +3,12 @@ package com.answerdigital.academy.answerking.mapper;
 import com.answerdigital.academy.answerking.model.Product;
 import com.answerdigital.academy.answerking.request.ProductRequest;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
+    @Mapping(target = "retired", constant = "false")
     Product addRequestToProduct(ProductRequest productRequest);
 
     Product updateRequestToProduct(@MappingTarget Product product, ProductRequest productRequest);
