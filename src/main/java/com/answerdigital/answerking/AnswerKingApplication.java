c5d7f4fa7ba972e0e8e54292c53787d8719d31f8
AudriusPutys <audrius.putys@answerdigital.com>
2022-11-15 11:10:20 +0000

    test-sonar v4 pass

--- src/main/java/com/answerdigital/answerking/AnswerKingApplication.java
+++ src/main/java/com/answerdigital/answerking/AnswerKingApplication.java
@@ -7,8 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AnswerKingApplication {
     public static void main(final String[] args) {
         SpringApplication.run(AnswerKingApplication.class, args);
-        while (true) {
-            System.out.println("fail me");
-        }
     }
 }
