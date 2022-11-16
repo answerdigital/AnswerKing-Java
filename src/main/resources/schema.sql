43b3ca3b3257c37d7ef42e1b17bca0bc7f48c0d7
Oleksii <oleksi.rybak@gmail.com>
2022-11-16 10:55:37 +0000

    adding missing fields as per API spec

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -12,6 +12,8 @@ CREATE TABLE IF NOT EXISTS category (
     id BIGINT NOT NULL AUTO_INCREMENT,
     name VARCHAR(255),
     description VARCHAR(255) NOT NULL,
+    createdOn VARCHAR(255) NOT NULL,
+    lastUpdated VARCHAR(255) NOT NULL,
     retired BIT(1) NOT NULL,
 
     PRIMARY KEY (id)
