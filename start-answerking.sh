82f8b844d3aba1a5458df027bd114bddb1eacfa8
martintaylor1635 <martintaylor1635@gmail.com>
2022-11-16 15:30:47 +0000

    BENCH-185 Updated bash script to initialise Docker

--- start-answerking.sh
+++ start-answerking.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# If this script does not work for you please ensure you install
+# Maven onto your machine, https://maven.apache.org/install.html
+
+echo "================================================"
+echo "Beginning Docker Build"
+echo "================================================"
+
 docker-compose down --remove-orphans
 mvn clean install
 docker-compose build
