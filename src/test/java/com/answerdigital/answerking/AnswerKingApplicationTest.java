9ba430e0497ebdeeadeb63302473259e2da6bcb8
martintaylor1635 <martintaylor1635@gmail.com>
2022-11-17 14:08:56 +0000

    BENCH-187-TESTCONTAINERS Added a basic IT to test is MySQL container is able to connect

---
+++ src/test/java/com/answerdigital/answerking/AnswerKingApplicationTest.java
@@ -0,0 +1,14 @@
+package com.answerdigital.answerking;
+
+import com.answerdigital.answerking.utility.ContainerisedTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnswerKingApplicationTest extends ContainerisedTest
+{
+    @Test
+    void contextLoads(){
+
+    }
+}
