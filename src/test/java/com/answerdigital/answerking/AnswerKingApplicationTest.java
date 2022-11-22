a41093db8c44a4b543f3e2420b59a7e025cad1b2
AudriusPutys <Audrius.Putys@answerdigital.com>
2022-11-22 10:44:37 +0000

    BENCH-187 Example Integration test added

--- src/test/java/com/answerdigital/answerking/AnswerKingApplicationTest.java
+++ src/test/java/com/answerdigital/answerking/AnswerKingApplicationTest.java
@@ -1,14 +1,39 @@
 package com.answerdigital.answerking;
 
 import com.answerdigital.answerking.utility.AbstractContainerBaseTest;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
 class AnswerKingApplicationTest extends AbstractContainerBaseTest
 {
-    @Test
-    void contextLoads(){
+    @Autowired
+    private MockMvc mockMvc;
 
+    @Test
+    void getAllProductsReturnListOfProductObjects() throws Exception {
+        //when
+        RequestBuilder request = MockMvcRequestBuilders.get("/products");
+        MockHttpServletResponse response = mockMvc.perform(request).andReturn().getResponse();
+
+        //then
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+        assertFalse(response.getContentAsString().isEmpty());
+        ObjectMapper mapper = new ObjectMapper();
+        assertEquals("Burger", mapper.readTree(response.getContentAsString()).get(0).get("name").textValue());
+        assertEquals("300g Beef", mapper.readTree(response.getContentAsString()).get(0).get("description").textValue());
+        assertEquals(6.69, mapper.readTree(response.getContentAsString()).get(0).get("price").asDouble());
     }
 }
