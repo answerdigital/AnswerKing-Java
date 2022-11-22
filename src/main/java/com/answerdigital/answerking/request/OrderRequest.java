d531709be7985694d899081dc7924ab97c455513
martintaylor1635 <martintaylor1635@gmail.com>
2022-11-22 15:28:19 +0000

    BENCH-182 Removed redundant valid annotation from OrderRequest.java

--- src/main/java/com/answerdigital/answerking/request/OrderRequest.java
+++ src/main/java/com/answerdigital/answerking/request/OrderRequest.java
@@ -1,9 +1,7 @@
 package com.answerdigital.answerking.request;
 
-import javax.validation.Valid;
 import java.util.List;
 
 public record OrderRequest(
-        @Valid
         List<LineItemRequest> lineItems
 ) { }
