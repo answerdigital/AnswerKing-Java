d85d634c484908cdd8dfd9f46dd39e7ee1e5712a
Andy Flint <andrew.flint@answerdigital.com>
2022-11-22 11:42:35 +0000

    BENCH-205 removed unused import

--- src/main/java/com/answerdigital/answerking/model/Product.java
+++ src/main/java/com/answerdigital/answerking/model/Product.java
@@ -1,6 +1,5 @@
 package com.answerdigital.answerking.model;
 
-import com.answerdigital.answerking.request.ProductRequest;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Builder;
