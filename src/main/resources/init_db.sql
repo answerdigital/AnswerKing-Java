f83d8faef55c36e05d770c217a5c43428b05e9b0
martintaylor1635 <martintaylor1635@gmail.com>
2022-11-22 10:40:27 +0000

    BENCH-187-TEST-CONTAINERS Added formatting to init_db.sql

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -7,11 +7,11 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `category`;
 CREATE TABLE `category`  (
-                             `id` bigint(20) NOT NULL AUTO_INCREMENT,
-                             `name` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-                             `description` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-                             `retired` bit(1) NOT NULL,
-                             PRIMARY KEY (`id`) USING BTREE
+    `id` bigint(20) NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+    `description` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+    `retired` bit(1) NOT NULL,
+    PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
@@ -23,10 +23,10 @@ CREATE TABLE `category`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `order`;
 CREATE TABLE `order`  (
-                          `id` bigint(20) NOT NULL AUTO_INCREMENT,
-                          `address` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-                          `order_status` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-                          PRIMARY KEY (`id`) USING BTREE
+    `id` bigint(20) NOT NULL AUTO_INCREMENT,
+    `address` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+    `order_status` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+    PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
@@ -40,12 +40,12 @@ INSERT INTO `order` VALUES (2, 'Manchester', 'IN_PROGRESS');
 -- ----------------------------
 DROP TABLE IF EXISTS `product`;
 CREATE TABLE `product`  (
-                            `id` bigint(20) NOT NULL AUTO_INCREMENT,
-                            `name` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-                            `description` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-                            `retired` bit(1) NOT NULL,
-                            `price` decimal(12, 2) NOT NULL,
-                            PRIMARY KEY (`id`) USING BTREE
+    `id` bigint(20) NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+    `description` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+    `retired` bit(1) NOT NULL,
+    `price` decimal(12, 2) NOT NULL,
+    PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
@@ -60,15 +60,15 @@ INSERT INTO `product` VALUES (3, 'Fries', 'Large Fries', b'0', 2.99);
 -- ----------------------------
 DROP TABLE IF EXISTS `order_product`;
 CREATE TABLE `order_product`  (
-                                  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-                                  `order_id` bigint(20) NOT NULL,
-                                  `product_id` bigint(20) NOT NULL,
-                                  `quantity` int(11) NOT NULL,
-                                  PRIMARY KEY (`id`) USING BTREE,
-                                  INDEX `order_id`(`order_id`) USING BTREE,
-                                  INDEX `product_id`(`product_id`) USING BTREE,
-                                  CONSTRAINT `order_product_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `order` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-                                  CONSTRAINT `order_product_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+    `id` bigint(20) NOT NULL AUTO_INCREMENT,
+    `order_id` bigint(20) NOT NULL,
+    `product_id` bigint(20) NOT NULL,
+    `quantity` int(11) NOT NULL,
+    PRIMARY KEY (`id`) USING BTREE,
+    INDEX `order_id`(`order_id`) USING BTREE,
+    INDEX `product_id`(`product_id`) USING BTREE,
+    CONSTRAINT `order_product_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `order` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+    CONSTRAINT `order_product_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
@@ -84,14 +84,14 @@ INSERT INTO `order_product` VALUES (4, 2, 3, 1);
 -- ----------------------------
 DROP TABLE IF EXISTS `product_category`;
 CREATE TABLE `product_category`  (
-                                     `id` bigint(20) NOT NULL AUTO_INCREMENT,
-                                     `product_id` bigint(20) NOT NULL,
-                                     `category_id` bigint(20) NOT NULL,
-                                     PRIMARY KEY (`id`) USING BTREE,
-                                     INDEX `product_id`(`product_id`) USING BTREE,
-                                     INDEX `category_id`(`category_id`) USING BTREE,
-                                     CONSTRAINT `product_category_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-                                     CONSTRAINT `product_category_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+    `id` bigint(20) NOT NULL AUTO_INCREMENT,
+    `product_id` bigint(20) NOT NULL,
+    `category_id` bigint(20) NOT NULL,
+    PRIMARY KEY (`id`) USING BTREE,
+    INDEX `product_id`(`product_id`) USING BTREE,
+    INDEX `category_id`(`category_id`) USING BTREE,
+    CONSTRAINT `product_category_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+    CONSTRAINT `product_category_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
