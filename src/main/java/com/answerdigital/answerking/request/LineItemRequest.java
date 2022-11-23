ca96d53f56aa631a423aa3d6895beb4d1c66acd2
martintaylor1635 <martintaylor1635@gmail.com>
2022-11-23 15:37:52 +0000

    BENCH-182 Refactored Java bean validation annotations

--- src/main/java/com/answerdigital/answerking/request/LineItemRequest.java
+++ src/main/java/com/answerdigital/answerking/request/LineItemRequest.java
@@ -1,11 +1,10 @@
 package com.answerdigital.answerking.request;
 
 import javax.validation.constraints.Positive;
-import javax.validation.constraints.PositiveOrZero;
 
 public record LineItemRequest(
         @Positive(message = "The product ID must be a positive number")
         Long productId,
-        @PositiveOrZero(message = "The quantity must be must be greater than or equal to 0")
+        @Positive(message = "The quantity must be a positive number")
         Integer quantity
 ) { }
