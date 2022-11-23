64aaa6e0c35d0b4f32721b3a765ad68bd100be5d
martintaylor1635 <martintaylor1635@gmail.com>
2022-11-23 12:13:24 +0000

    BENCH-182 Removed unused import

--- src/main/java/com/answerdigital/answerking/model/Order.java
+++ src/main/java/com/answerdigital/answerking/model/Order.java
@@ -17,7 +17,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.persistence.Table;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalDateTime;
