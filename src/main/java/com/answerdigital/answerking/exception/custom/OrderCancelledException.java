51295983020692511b7af605eed1176442ab7ae3
martintaylor1635 <martintaylor1635@gmail.com>
2022-11-28 13:58:51 +0000

    BENCH-182-TEST Refactored OrderCancelledException.java

--- src/main/java/com/answerdigital/answerking/exception/custom/OrderCancelledException.java
+++ src/main/java/com/answerdigital/answerking/exception/custom/OrderCancelledException.java
@@ -3,9 +3,6 @@ package com.answerdigital.answerking.exception.custom;
 import com.answerdigital.answerking.exception.AnswerKingException;
 import org.springframework.http.HttpStatus;
 
-import java.util.Collection;
-import java.util.List;
-
 public class OrderCancelledException extends AnswerKingException {
     private static final String TYPE = "https://www.rfc-editor.org/rfc/rfc7231";
 
@@ -13,13 +10,7 @@ public class OrderCancelledException extends AnswerKingException {
 
     private static final HttpStatus STATUS = HttpStatus.BAD_REQUEST;
 
-    private static final String DETAIL = null;
-
-    public OrderCancelledException(final Collection<String> errors) {
-        super(TYPE, TITLE, STATUS, DETAIL, errors);
-    }
-
     public OrderCancelledException(final String error) {
-        this(List.of(error));
+        super(TYPE, TITLE, STATUS, error);
     }
 }
