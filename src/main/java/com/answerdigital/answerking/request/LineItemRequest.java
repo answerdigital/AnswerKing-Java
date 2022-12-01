b89b06273cbbeb93fea1fe760b2178484086b031
martintaylor1635 <martintaylor1635@gmail.com>
2022-12-01 14:04:53 +0000

    BENCH-182 Added equals and hashcode to LineItemRequest.java to check for equality on ID

--- src/main/java/com/answerdigital/answerking/request/LineItemRequest.java
+++ src/main/java/com/answerdigital/answerking/request/LineItemRequest.java
@@ -1,10 +1,24 @@
 package com.answerdigital.answerking.request;
 
 import javax.validation.constraints.Positive;
+import java.util.Objects;
 
 public record LineItemRequest(
         @Positive(message = "The product ID must be a positive number")
         Long productId,
         @Positive(message = "The quantity must be a positive number")
         Integer quantity
-) { }
+) {
+        @Override
+        public boolean equals(Object o) {
+                if (this == o) return true;
+                if (o == null || getClass() != o.getClass()) return false;
+                LineItemRequest that = (LineItemRequest) o;
+                return productId.equals(that.productId);
+        }
+
+        @Override
+        public int hashCode() {
+                return Objects.hash(productId);
+        }
+}
