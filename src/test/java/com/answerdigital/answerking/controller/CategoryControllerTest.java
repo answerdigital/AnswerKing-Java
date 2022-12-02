f806e0ef0de51a411e221c7e5cc80171c7eb12f3
martintaylor1635 <martintaylor1635@gmail.com>
2022-12-02 14:48:54 +0000

    BENCH-249 Checkstyle

--- src/test/java/com/answerdigital/answerking/controller/CategoryControllerTest.java
+++ src/test/java/com/answerdigital/answerking/controller/CategoryControllerTest.java
@@ -26,7 +26,10 @@ import java.time.temporal.ChronoUnit;
 import java.util.HashSet;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.doReturn;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
