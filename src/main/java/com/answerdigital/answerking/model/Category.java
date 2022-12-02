a9a9bd0ff5e527ea0a1fac44d378d9f58780e64c
martintaylor1635 <martintaylor1635@gmail.com>
2022-12-02 12:47:44 +0000

    BENCH-249 Checkstyle

--- src/main/java/com/answerdigital/answerking/model/Category.java
+++ src/main/java/com/answerdigital/answerking/model/Category.java
@@ -10,7 +10,13 @@ import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import java.time.LocalDateTime;
