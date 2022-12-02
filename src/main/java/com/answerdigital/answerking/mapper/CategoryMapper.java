6b400ec7af3ad9cd413c1ddc2e88cdad08c58485
martintaylor1635 <martintaylor1635@gmail.com>
2022-12-02 12:05:26 +0000

    BENCH-249 Modifed CategoryMapper.java to reflect LocalDateTime changes.

--- src/main/java/com/answerdigital/answerking/mapper/CategoryMapper.java
+++ src/main/java/com/answerdigital/answerking/mapper/CategoryMapper.java
@@ -3,25 +3,22 @@ package com.answerdigital.answerking.mapper;
 import com.answerdigital.answerking.model.Category;
 import com.answerdigital.answerking.request.CategoryRequest;
 import com.answerdigital.answerking.response.CategoryResponse;
-import com.answerdigital.answerking.util.DateTimeUtility;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+
+import java.util.Collections;
 import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring",
-        imports = {DateTimeUtility.class, Collectors.class})
+        imports = {Collectors.class, Collections.class})
 public interface CategoryMapper {
-
-    @Mapping(target = "createdOn", expression = "java(DateTimeUtility.getDateTimeAsString())")
-    @Mapping(target = "lastUpdated", expression = "java(DateTimeUtility.getDateTimeAsString())")
+    @Mapping(target = "products", expression = "java(Collections.EMPTY_SET)")
     Category addRequestToCategory(CategoryRequest addCategoryRequest);
 
-    @Mapping(target = "lastUpdated", expression = "java(DateTimeUtility.getDateTimeAsString())")
     Category updateRequestToCategory(@MappingTarget Category category, CategoryRequest updateCategoryRequest);
 
     @Mapping(target = "products",
              expression = "java(category.getProducts().stream().map(product -> product.getId()).collect(Collectors.toList()) )")
     CategoryResponse convertCategoryEntityToCategoryResponse(Category category);
-
 }
