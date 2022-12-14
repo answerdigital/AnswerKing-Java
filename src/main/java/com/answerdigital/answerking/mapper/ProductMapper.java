4ce25da6238e7dcac02a2868461900503b935e6e
martintaylor1635 <martintaylor1635@gmail.com>
2022-12-14 09:36:31 +0000

    BENCH-256 Updated mappers to use the productIds list name rather than the previous products list name

--- src/main/java/com/answerdigital/answerking/mapper/ProductMapper.java
+++ src/main/java/com/answerdigital/answerking/mapper/ProductMapper.java
@@ -6,15 +6,19 @@ import com.answerdigital.answerking.response.ProductResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
-import java.util.List;
 
-@Mapper(componentModel = "spring", imports = {List.class})
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Mapper(componentModel = "spring", imports = {List.class, Collectors.class})
 public interface ProductMapper {
     @Mapping(target = "retired", constant = "false")
     Product addRequestToProduct(ProductRequest productRequest);
 
     Product updateRequestToProduct(@MappingTarget Product product, ProductRequest productRequest);
 
+    @Mapping(target = "category.productIds",
+            expression = "java(category.getProducts().stream().map(product -> product.getId()).collect(Collectors.toList()) )")
     ProductResponse convertProductEntityToProductResponse(Product product);
 
 }
