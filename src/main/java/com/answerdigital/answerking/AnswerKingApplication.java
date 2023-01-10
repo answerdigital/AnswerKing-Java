2176a6fb6ba49a746d68c302c93264a6b4839d61
martintaylor1635 <martintaylor1635@gmail.com>
2023-01-10 10:38:01 +0000

    BENCH-294 Removed explicit exclude to `SecurityAutoConfiguration.class` and added `@Configuration` to SecurityConfig.java

--- src/main/java/com/answerdigital/answerking/AnswerKingApplication.java
+++ src/main/java/com/answerdigital/answerking/AnswerKingApplication.java
@@ -2,9 +2,8 @@ package com.answerdigital.answerking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class AnswerKingApplication {
     public static void main(final String[] args) {
 	SpringApplication.run(AnswerKingApplication.class, args);
--- src/main/java/com/answerdigital/answerking/config/SecurityConfig.java
+++ src/main/java/com/answerdigital/answerking/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.answerdigital.answerking.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -10,6 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.stream.Stream;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     private static final String[] PERMITTED_PATTERNS = {
