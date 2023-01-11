698ea2449951c4f270be8f9c4950ee5a3177d37e
Oleksii <oleksi.rybak@gmail.com>
2023-01-11 13:44:00 +0000

    add answer king java

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,13 +2,8 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "answer_king_java" {
   count         = 1
   ami           = "ami-084e8c05825742534"
   instance_type = "t2.micro"
-
-  tags {
-    Name = "answer-king-example"
-  }
-
 }
