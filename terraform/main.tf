4ca706ed6615338f566feb66d522cbeff5fcc3ff
Oleksii <oleksi.rybak@gmail.com>
2023-01-12 16:25:51 +0000

    adding docker

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,4 +6,12 @@ resource "aws_instance" "answer_king_java" {
   count         = 1
   ami           = "ami-084e8c05825742534"
   instance_type = "t2.micro"
+
+  user_data = <<-EOF
+                #!/bin/bash
+                sudo yum install docker -y
+                sudo service docker start
+              EOF
+
+  user_data_replace_on_change = true
 }
