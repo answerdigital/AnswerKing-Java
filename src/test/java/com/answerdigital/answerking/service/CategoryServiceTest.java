1494e2a8ab76ff929777714022e3e429083e1a0b
Oleksii <oleksi.rybak@gmail.com>
2023-01-13 13:24:43 +0000

    rebase

--- src/test/java/com/answerdigital/answerking/service/CategoryServiceTest.java
+++ src/test/java/com/answerdigital/answerking/service/CategoryServiceTest.java
@@ -272,7 +272,7 @@ final class CategoryServiceTest {
         // when
         when(categoryRepository.existsByNameAndIdIsNot(anyString(), anyLong())).thenReturn(true);
         final Exception exception = assertThrows(NameUnavailableException.class,
-                () -> categoryService.updateCategory(updateCategoryRequest, existingCategoryId));
+            () -> categoryService.updateCategory(updateCategoryRequest, existingCategoryId));
 
         // then
         assertFalse(exception.getMessage().isEmpty());
@@ -302,8 +302,8 @@ final class CategoryServiceTest {
     void testRetireCategoryAlreadyRetiredThrowsRetirementException() {
         // given
         final Category retiredCategory = categoryTestBuilder.withDefaultValues()
-                .withRetired(true)
-                .build();
+            .withRetired(true)
+            .build();
 
         final Long retiredCategoryId = retiredCategory.getId();
 
