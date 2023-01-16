0b25a8f23b9081838ff113ebf63882b82c11dac0
Oleksii <oleksi.rybak@gmail.com>
2023-01-16 14:31:03 +0000

    link ec2 with security group

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,9 +3,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "answer_king_java" {
-  count         = 1
-  ami           = "ami-084e8c05825742534"
-  instance_type = "t2.micro"
+  count                  = 1
+  ami                    = "ami-084e8c05825742534"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
   user_data = <<-EOF
                 #!/bin/bash
