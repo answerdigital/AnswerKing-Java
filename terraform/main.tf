a06763cc166439886758f1984dcedf592b12ecfd
Oleksii <oleksi.rybak@gmail.com>
2023-01-16 13:28:31 +0000

    add docker build

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,6 +11,9 @@ resource "aws_instance" "answer_king_java" {
                 #!/bin/bash
                 sudo yum install docker -y
                 sudo service docker start
+                docker pull ghcr.io/answerconsulting/answerking-java/answer-king-rest-api_app:latest
+                docker-compose build
+                docker-compose up
               EOF
 
   user_data_replace_on_change = true
