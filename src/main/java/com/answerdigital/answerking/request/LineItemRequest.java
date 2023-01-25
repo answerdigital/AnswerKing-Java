67386bc52ca19ac48d9f8bef610b5198536c43e7
AudriusPutys <Audrius.Putys@answerdigital.com>
2023-01-25 16:58:50 +0000

    BENCH-337 Test Coverage improvement

--- src/main/java/com/answerdigital/answerking/request/LineItemRequest.java
+++ src/main/java/com/answerdigital/answerking/request/LineItemRequest.java
@@ -1,7 +1,6 @@
 package com.answerdigital.answerking.request;
 
 import jakarta.validation.constraints.Positive;
-import java.util.Objects;
 
 public record LineItemRequest(
         @Positive(message = "The product ID must be a positive number")
@@ -9,16 +8,4 @@ public record LineItemRequest(
         @Positive(message = "The quantity must be a positive number")
         Integer quantity
 ) {
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        final LineItemRequest that = (LineItemRequest) o;
-        return productId.equals(that.productId);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(productId);
-    }
 }
---
+++ src/test/java/com/answerdigital/answerking/response/OrderResponseTest.java
@@ -0,0 +1,45 @@
+package com.answerdigital.answerking.response;
+
+import com.answerdigital.answerking.builder.LineItemResponseTestBuilder;
+import com.answerdigital.answerking.builder.ProductResponseTestBuilder;
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class OrderResponseTest {
+    private final ProductResponseTestBuilder productResponseTestBuilder = new ProductResponseTestBuilder();
+
+    private final LineItemResponseTestBuilder lineItemResponseTestBuilder = new LineItemResponseTestBuilder();
+
+    @Test
+    public void testOrderTotalWhenBasketIsEmptyIsZero() {
+        final OrderResponse orderResponse = new OrderResponse();
+        orderResponse.setLineItems(new HashSet<>());
+        assertEquals("0.00", orderResponse.getOrderTotal().toString());
+    }
+
+    @Test
+    public void testGetOrderTotalSuccessfully() {
+        final ProductResponse product = productResponseTestBuilder.withDefaultValues().build();
+        final LineItemResponse lineItemResponse = lineItemResponseTestBuilder
+                .withProductResponse(product)
+                .withQuantity(1)
+                .build();
+        final LineItemResponse lineItemResponseTwo = lineItemResponseTestBuilder
+                .withProductResponse(product)
+                .withQuantity(10)
+                .build();
+
+        final OrderResponse orderResponse = new OrderResponse();
+        orderResponse.setLineItems(Set.of(lineItemResponse, lineItemResponseTwo));
+        assertEquals("142.89", orderResponse.getOrderTotal().toString());
+        assertEquals("12.99", orderResponse.getLineItems().stream().findFirst().get().getSubTotal().toString());
+    }
+}
+
