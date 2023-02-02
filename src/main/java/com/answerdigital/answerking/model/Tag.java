82ff15500ba71c16eaf2a12cb4374b38b777b3f7
martintaylor1635 <martintaylor1635@gmail.com>
2023-02-02 11:30:05 +0000

    BENCH-328 Updated Tag.java to remove star import as per checkstyle constraints

--- src/main/java/com/answerdigital/answerking/model/Tag.java
+++ src/main/java/com/answerdigital/answerking/model/Tag.java
@@ -1,6 +1,15 @@
 package com.answerdigital.answerking.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
