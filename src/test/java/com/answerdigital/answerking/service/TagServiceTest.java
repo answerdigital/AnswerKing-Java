a544b812d29addf1e498897693b1041d9f35f2c8
martintaylor1635 <martintaylor1635@gmail.com>
2023-02-02 16:17:36 +0000

    BENCH-328 Checkstyle, again 🙄

--- src/test/java/com/answerdigital/answerking/service/TagServiceTest.java
+++ src/test/java/com/answerdigital/answerking/service/TagServiceTest.java
@@ -25,7 +25,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyList;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class TagServiceTest {
