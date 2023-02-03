94a47c6ca9fd3453766ae5daccb832c11a201322
martintaylor1635 <martintaylor1635@gmail.com>
2023-02-03 10:41:53 +0000

    BENCH-328 Refactored OrderControllerTest.java, added mapping utility to MappingUtility.java

--- src/test/java/com/answerdigital/answerking/utility/MappingUtility.java
+++ src/test/java/com/answerdigital/answerking/utility/MappingUtility.java
@@ -1,7 +1,10 @@
 package com.answerdigital.answerking.utility;
 
+import com.answerdigital.answerking.mapper.OrderMapper;
 import com.answerdigital.answerking.mapper.TagMapper;
+import com.answerdigital.answerking.model.Order;
 import com.answerdigital.answerking.model.Tag;
+import com.answerdigital.answerking.response.OrderResponse;
 import com.answerdigital.answerking.response.TagResponse;
 import org.mapstruct.factory.Mappers;
 
@@ -11,9 +14,25 @@ import java.util.List;
 public final class MappingUtility {
     private static final TagMapper TAG_MAPPER = Mappers.getMapper(TagMapper.class);
 
+    private static final OrderMapper ORDER_MAPPER = Mappers.getMapper(OrderMapper.class);
+
+    /**
+     * Takes <i>n</i> arguments of type Tag and converts them to a TagResponse.
+     * @param tags A Collection of type {@link com.answerdigital.answerking.model.Tag}.
+     * @return A List of type {@link com.answerdigital.answerking.response.TagResponse}.
+     */
     public static List<TagResponse> allTagsToResponse(final Tag ...tags) {
         return Arrays.stream(tags)
             .map(TAG_MAPPER::convertTagEntityToTagResponse)
             .toList();
+    }
+
+    /**
+     * Takes an Order and converts it to and OrderResponse.
+     * @param order
+     * @return
+     */
+    public static OrderResponse orderToResponse(final Order order) {
+        return ORDER_MAPPER.orderToOrderResponse(order);
     }
 }
