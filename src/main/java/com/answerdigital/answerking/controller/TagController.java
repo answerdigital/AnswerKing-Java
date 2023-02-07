5874c64d7acc993c5004d5ea1629c5bcf3cf3773
Andy Flint <andrew.flint@answerdigital.com>
2023-02-07 11:45:04 +0000

    BENCH-328 remove unused import

--- src/main/java/com/answerdigital/answerking/controller/TagController.java
+++ src/main/java/com/answerdigital/answerking/controller/TagController.java
@@ -16,7 +16,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
